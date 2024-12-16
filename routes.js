6adf06f9ab59190714994d8800c114e89b7e5467
Den Omelchenko <dedomelchenko@gmail.com>
2024-12-16 22:46:35 +0200

    add file

--- routes.js
+++ routes.js
@@ -378,7 +378,7 @@ router.post('/forgotPassword', async (req, res) => {
                 subject: 'Password Reset',
                 html: `<p>You are receiving this email because you (or someone else) have requested the reset of the password for your account.</p>
                        <p>Please click on the following link, or paste this into your browser to complete the process:</p>
-                       <p>https://jovial-blini-d5bb4d.netlify.app/resetPassword?token=${token}</p>
+                       <p>https://loquacious-quokka-042395.netlify.app/resetPassword?token=${token}</p>
                        <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>`
             };
         
